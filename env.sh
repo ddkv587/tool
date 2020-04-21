37ba5de9cf6145e6e18df27bcc7d35164332987c
ddkV587 <ddkV587@gmail.com>
2020-04-21 11:27:22 +0800

    Initial commit

---
+++ env.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+sudo apt-get update
+
+# c / c++ / python
+sudo apt-get install -y gcc g++ gcc-multilib g++-multilib build-essential python3 python3-pip
+
+# version
+sudo apt-get install -y git git-core
+
+# ide
+sudo apt-get install -y vim ctags
+
+# library
+#sudo apt-get install -y  
+
+# tool
+sudo apt-get install -y ssh zip curl wireshark
